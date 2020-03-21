b7bf858d179f3e65c1d210ee3b65271721a17b41
yagmurcetintas <yagmurcetin@gmail.com>
2020-03-21 18:08:03 -0400

    added routing and navbar style

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import "./App.css";
 import Status from "./components/status/status";
 import Navbar from "./components/navbar/navbar";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Status />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" component={Status} />
+          <Route path="/protection" component={Status} />
+          <Route path="/articles" component={Status} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
