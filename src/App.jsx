f53a6a6edf94855fc82115ac70b16c9c40128365
yagmurcetintas <yagmurcetin@gmail.com>
2020-03-21 16:37:54 -0400

    added primitive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Status from "./components/status/status";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Status />
     </div>
   );
