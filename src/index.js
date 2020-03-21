3abccaebf9c9ea7642b79f9eaccc3b99618b5af5
Ali Tas <ali.tas@edelkrone.com>
2020-03-21 16:07:23 -0400

    Formatted index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render( <
-  React.StrictMode >
-  <
-  App / >
-  <
-  /React.StrictMode>,
+ReactDOM.render(<React.StrictMode>
+  <App />
+</React.StrictMode>,
   document.getElementById("root")
 );
 
