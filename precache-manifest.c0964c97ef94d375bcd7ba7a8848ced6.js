54746f3be31554db73237dfceaed1c9e2380d0b8
yagmurcetintas <yagmurcetin@gmail.com>
2020-03-22 18:45:03 -0400

    Updates

---
+++ precache-manifest.c0964c97ef94d375bcd7ba7a8848ced6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5624f600f4ad8007595146170820e75",
+    "url": "/whereiscovid/index.html"
+  },
+  {
+    "revision": "a8f7e1db2edace9066d7",
+    "url": "/whereiscovid/static/css/main.672cbdbc.chunk.css"
+  },
+  {
+    "revision": "524c86f6568528fec7a1",
+    "url": "/whereiscovid/static/js/2.12f53a82.chunk.js"
+  },
+  {
+    "revision": "031b62b957b4fb917cd0ec81e2c8bd7b",
+    "url": "/whereiscovid/static/js/2.12f53a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f7e1db2edace9066d7",
+    "url": "/whereiscovid/static/js/main.5cfeabdd.chunk.js"
+  },
+  {
+    "revision": "87d36fb177d60976a45b",
+    "url": "/whereiscovid/static/js/runtime-main.97e00128.js"
+  },
+  {
+    "revision": "65fe8e5b75e58b06e88480ade292ad28",
+    "url": "/whereiscovid/static/media/rotatedTitle.65fe8e5b.png"
+  },
+  {
+    "revision": "808129791a400bfad4c47341347c9727",
+    "url": "/whereiscovid/static/media/tree.80812979.jpg"
+  }
+]);
