78f3a58af56d53fe06e302e19975c37940670f38
Ali Tas <ali.tas@edelkrone.com>
2020-03-22 15:07:23 -0400

    Added link to CDC guidelines

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import classes from "./navbar.module.css";
 import { Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = props => (
   <div className={classes.navbar}>
@@ -38,18 +40,6 @@ const Navbar = props => (
       </NavLink>
       <NavLink
         className={classes.navbar__item}
-        to="/protection"
-        activeClassName="navbar__item--active"
-        activeStyle={{
-          backgroundColor: "var(--gray)",
-          color: "white",
-          fontWeight: "bold"
-        }}
-      >
-        Protection
-      </NavLink>
-      <NavLink
-        className={classes.navbar__item}
         to="/articles"
         activeClassName="navbar__item--active"
         activeStyle={{
@@ -60,6 +50,14 @@ const Navbar = props => (
       >
         Articles
       </NavLink>
+      <a
+        className={classes.navbar__item}
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.cdc.gov/coronavirus/2019-ncov/index.html"
+      >
+        CDC Guidelines <FontAwesomeIcon icon={faExternalLinkAlt} />
+      </a>
     </div>
   </div>
 );
