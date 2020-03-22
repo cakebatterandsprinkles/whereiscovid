a52e83213205c1ff5f013cc9986819cc1f5b10ba
yagmurcetintas <yagmurcetin@gmail.com>
2020-03-21 22:26:31 -0400

    added map marker styling logic

--- src/components/countryMarker/countryMarker.jsx
+++ src/components/countryMarker/countryMarker.jsx
@@ -2,12 +2,47 @@ import React, { PureComponent } from "react";
 import classes from "./countryMarker.module.css";
 
 class CountryMarker extends PureComponent {
-    render() {
-        if (this.props.data[this.props.sortBy] === 0) {
-            return null;
-        }
-        return (<div className={classes.marker}>{this.props.data[this.props.sortBy].toLocaleString()}</div>);
+  chooseColor = value => {
+    if (value === "cases" || value === "todayCases") {
+      return classes.red;
+    } else if (value === "deaths" || value === "todayDeaths") {
+      return classes.gray;
+    } else if (value === "recovered") {
+      return classes.green;
+    } else if (value === "active") {
+      return classes.yellow;
+    } else if (value === "critical") {
+      return classes.orange;
+    } else if (value === "casesPerOneMillion") {
+      return classes.purple;
     }
+    return classes.red;
+  };
+  calculateSize = value => {
+    if (value < 100) {
+      return classes.small;
+    } else if (value < 500) {
+      return classes.medium;
+    } else if (value < 2000) {
+      return classes.large;
+    } else if (value >= 2000) {
+      return classes.x_large;
+    }
+  };
+  render() {
+    const displayedValue = this.props.data[this.props.sortBy];
+    if (displayedValue === 0) {
+      return null;
+    }
+    const sizeClass = this.calculateSize(displayedValue);
+    const colorClass = this.chooseColor(this.props.sortBy);
+
+    return (
+      <div className={`${classes.marker} ${sizeClass} ${colorClass}`}>
+        {displayedValue.toLocaleString()}
+      </div>
+    );
+  }
 }
 
 export default CountryMarker;
