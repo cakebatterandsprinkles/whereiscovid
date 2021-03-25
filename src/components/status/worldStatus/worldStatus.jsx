37d1b834accd6b9f0243fe647b07bc93f9a5860b
yagmurcetintas <yagmurcetin@gmail.com>
2021-03-25 14:50:36 -0400

    Optimized data table for smaller screens and changed style

--- src/components/status/worldStatus/worldStatus.jsx
+++ src/components/status/worldStatus/worldStatus.jsx
@@ -2,11 +2,37 @@ import { faSort } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import GoogleMapReact from "google-map-react";
 import React, { PureComponent } from "react";
-import RotatedTitle from "../../../assets/img/rotatedTitle.png";
 import countryCoordinates from "../../../data/countries.json";
 import CountryMarker from "../countryMarker/countryMarker";
 import InfoBox from "../infoBox/infoBox";
 import classes from "./worldStatus.module.css";
+
+const fields = [
+  "cases",
+  "todayCases",
+  "deaths",
+  "todayDeaths",
+  "casesPerOneMillion",
+  "deathsPerOneMillion",
+];
+
+const fieldNames = {
+  cases: "Cases",
+  todayCases: "Today's Cases",
+  deaths: "Deaths",
+  todayDeaths: "Today's Deaths",
+  casesPerOneMillion: "Cases per 1M",
+  deathsPerOneMillion: "Deaths per 1M",
+};
+
+const fieldStyles = {
+  cases: classes.active_case,
+  todayCases: classes.active_case,
+  deaths: classes.active_death,
+  todayDeaths: classes.active_death,
+  casesPerOneMillion: classes.active_casesPerMillion,
+  deathsPerOneMillion: classes.active_deathsPerMillion,
+};
 
 class Status extends PureComponent {
   retrieveData() {
@@ -67,7 +93,7 @@ class Status extends PureComponent {
         todayCases: sortedTodayCases,
         todayDeaths: sortedTodayDeaths,
         casesPerOneMillion: sortedCasesPerOneMillion,
-        deathsPerOneMillion: sortedDeathsPerOneMillion
+        deathsPerOneMillion: sortedDeathsPerOneMillion,
       },
     });
   }
@@ -112,7 +138,7 @@ class Status extends PureComponent {
         deaths: [],
         todayDeaths: [],
         casesPerOneMillion: [],
-        deathsPerOneMillion: []
+        deathsPerOneMillion: [],
       },
       sortBy: "cases",
       selectedCountry: null,
@@ -128,13 +154,16 @@ class Status extends PureComponent {
   }
 
   render() {
-    const values = this.state.mainData.map((country) => country[this.state.sortBy]);
+    const values = this.state.mainData.map(
+      (country) => country[this.state.sortBy]
+    );
     const minValue = Math.min(...values);
     const maxValue = Math.max(...values);
-    
+
     return (
-      <div>
+      <div className={classes.mainWrapper}>
         <div
+          className={classes.mapContainer}
           onClick={() => this.setState({ selectedCountry: null })}
           style={{ height: `${this.state.mapHeight}vh`, width: "100%" }}
         >
@@ -173,114 +202,74 @@ class Status extends PureComponent {
             ) : null}
           </GoogleMapReact>
         </div>
-        {this.state.isXSmallScreen ? null : (
-          <div className={classes.table__container}>
-            <p className={classes.table__info}>
-              {" "}
-              You can click on the headers to see data ordered for each
-              parameter.
-            </p>
-            <div className={classes.table__inner__container}>
-              <div className={classes.table__header__container}>
-                <img
-                  alt="table title"
-                  className={classes.table__header}
-                  src={RotatedTitle}
-                />
-              </div>
-              <table className={classes.table}>
-                <thead>
-                  <tr>
-                    <th>Country</th>
+
+        <div className={classes.table__container}>
+          <p className={classes.table__info}>
+            {" "}
+            Top 20 countries for each parameter are displayed on the table.
+          </p>
+          <select
+            className={classes.field__select}
+            onChange={(changeEvent) =>
+              this.setState({ sortBy: changeEvent.target.value })
+            }
+            value={this.state.sortBy}
+          >
+            {fields.map((field) => (
+              <option key={field} value={field}>
+                {fieldNames[field]}
+              </option>
+            ))}
+          </select>
+          <div className={classes.table__inner__container}>
+            {/* <div className={classes.table__header__container}>
+              <img
+                alt="table title"
+                className={classes.table__header}
+                src={RotatedTitle}
+              />
+            </div> */}
+            <table className={classes.table}>
+              <thead>
+                <tr>
+                  <th className={classes.country}>Country</th>
+                  {fields.map((field, index) => (
                     <th
                       className={
-                        this.state.sortBy === "cases"
-                          ? classes.active_case
-                          : null
+                        this.state.sortBy === field ? fieldStyles[field] : null
                       }
-                      onClick={() =>
-                        this.setState({
-                          sortBy: "cases",
-                        })
-                      }
+                      key={`${field}-${index}`}
+                      onClick={() => this.setState({ sortBy: field })}
                     >
-                      Cases <FontAwesomeIcon icon={faSort} />
+                      {fieldNames[field]} <FontAwesomeIcon icon={faSort} />
                     </th>
-                    <th
-                      className={
-                        this.state.sortBy === "todayCases"
-                          ? classes.active_case
-                          : null
-                      }
-                      onClick={() => this.setState({ sortBy: "todayCases" })}
-                    >
-                      Today&apos;s Cases <FontAwesomeIcon icon={faSort} />
-                    </th>
-                    <th
-                      className={
-                        this.state.sortBy === "deaths"
-                          ? classes.active_death
-                          : null
-                      }
-                      onClick={() => this.setState({ sortBy: "deaths" })}
-                    >
-                      Deaths <FontAwesomeIcon icon={faSort} />
-                    </th>
-                    <th
-                      className={
-                        this.state.sortBy === "todayDeaths"
-                          ? classes.active_death
-                          : null
-                      }
-                      onClick={() => this.setState({ sortBy: "todayDeaths" })}
-                    >
-                      Today&apos;s Deaths <FontAwesomeIcon icon={faSort} />
-                    </th>
-                    <th
-                      className={
-                        this.state.sortBy === "casesPerOneMillion"
-                          ? classes.active_casesPerMillion
-                          : null
-                      }
-                      onClick={() =>
-                        this.setState({ sortBy: "casesPerOneMillion" })
-                      }
-                    >
-                      Cases per 1M <FontAwesomeIcon icon={faSort} />
-                    </th>
-                    <th
-                      className={
-                        this.state.sortBy === "deathsPerOneMillion"
-                          ? classes.active_deathsPerMillion
-                          : null
-                      }
-                      onClick={() =>
-                        this.setState({ sortBy: "deathsPerOneMillion" })
-                      }
-                    >
-                      Deaths per 1M <FontAwesomeIcon icon={faSort} />
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.state.top10Cases[this.state.sortBy].map((country) => {
-                    return (
-                      <tr key={country.country}>
-                        <td>{country.country}</td>
-                        <td>{country.cases.toLocaleString()}</td>
-                        <td>{country.todayCases.toLocaleString()}</td>
-                        <td>{country.deaths.toLocaleString()}</td>
-                        <td>{country.todayDeaths.toLocaleString()}</td>
-                        <td>{country.casesPerOneMillion.toLocaleString()}</td>
-                        <td>{country.deathsPerOneMillion.toLocaleString()}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.top10Cases[this.state.sortBy].map((country) => {
+                  return (
+                    <tr key={country.country}>
+                      <td className={classes.country}>{country.country}</td>
+                      {fields.map((field) => (
+                        <td
+                          className={
+                            this.state.sortBy === field
+                              ? classes.active_row
+                              : null
+                          }
+                          key={field}
+                        >
+                          {country[field].toLocaleString()}
+                        </td>
+                      ))}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
-        )}
+        </div>
       </div>
     );
   }
